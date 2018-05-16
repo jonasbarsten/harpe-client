9b549b51966aa158d65c9be2ec3734fd8343cb21
Jonas Barsten <jonasbarsten@gmail.com>
2018-05-16 14:20:08 +0200

    Adding diagonal

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
 import React, { Component } from 'react';
-// import socketIOClient from 'socket.io-client';
-// import UDPPort from 'osc';
+import socketIOClient from 'socket.io-client';
 
 import './App.css';
 
 // For some reason, cannot use 127.0.0.1
-// const endpoint = window.location.hostname + ':4001';
-// const socket = socketIOClient(endpoint);
-
-
-// const udpPort = osc.UDPPort({
-//   localAddress: "10.0.255.255",
-//   localPort: 8050,
-//   metadata: true,
-//   broadcast: true
-// });
+const endpoint = window.location.hostname + ':4001';
+const socket = socketIOClient(endpoint);
 
 
 class App extends Component {
   state = {
-    response: false,
-    oscPortReady: false
+    response: false
   }
 
   runLedCommand(functionName, args) {
-    // socket.emit('runFunction', functionName, args);
+    socket.emit('runFunction', functionName, args);
   }
 
   restartServer() {
-    // socket.emit('restart');
+    socket.emit('restart');
   }
+
+  // oscSend(address, value) {
+  //   socket.emit('oscSend', address, value);
+  // }
 
   componentDidMount() {
-    // socket.on("FromAPI", data => this.setState({ response: data }));
-
-    // udpPort.open();
-
-    // udpPort.on("ready", () => {
-    //   this.setState({
-    //     oscPortReady: true
-    //   });
-    // });
-
+    socket.on("FromAPI", data => this.setState({ response: data }));
   }
+
   render() {
-    // const { response } = this.state;
-    // console.log(response);
+    const { response } = this.state;
+    console.log(response);
 
     return (
       <div className="App">
         <div onClick={() => this.runLedCommand('allOn', "0")}>All on</div>
         <div onClick={() => this.runLedCommand('allOff', "0")}>All off</div>
+        <div onClick={() => this.runLedCommand('rotatePuffDiagonally', "0", 1)}>Diagonal</div>
         <div onClick={() => this.restartServer()}>Restart</div>
       </div>
     );
