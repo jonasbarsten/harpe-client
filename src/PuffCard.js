5ae5fad6cce0e9b18a76921a1d31be5ba20986fc
Jonas Barsten <jonasbarsten@gmail.com>
2018-05-21 17:39:44 +0200

    Adding links to monitor and ola

--- src/PuffCard.js
+++ src/PuffCard.js
@@ -7,13 +7,17 @@ const PuffCard = (props) => {
 
   const ip = props.puff.ip;
   const lastSeen = moment(props.puff.lastSeen).format('HH:mm:ss');
+  const monitorUrl = `http://${ip}:8888`;
+  const olaUrl = `http://${ip}:9090`;
 
   return (
     <Col sm="6">
       <Card body className="text-center">
         <CardTitle>{ip}</CardTitle>
         <CardText>Last seen <Badge color="primary">{lastSeen}</Badge></CardText>
-        <Button onClick={() => props.childProps.history.push(`/puff/${ip}`)}>Go to puff</Button>
+        <Button style={{marginBottom: "8px"}} color="primary" onClick={() => props.childProps.history.push(`/puff/${ip}`)}>Config / test</Button>
+        <Button style={{marginBottom: "8px"}} color="primary" onClick={() => window.location = monitorUrl}>Monitor</Button>
+        <Button color="primary" onClick={() => window.location = olaUrl}>OLA</Button>
       </Card>
     </Col>
   );
